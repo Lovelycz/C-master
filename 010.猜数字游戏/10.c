f456125672bf014aa75094823f2542912e35343c
橙子 <132437912+Lovelycz@users.noreply.github.com>
2023-10-17 21:48:09 +0800

    Update 10.c

--- 010.猜数字游戏/10.c
+++ 010.猜数字游戏/10.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main() {
-    int Password = 0, Number = 0, price = 58, i = 0;
+int main()
+{
+	int Password = 0, Number = 0, price = 58, i = 0;
+	//Clear screen 
+	system("cls");
+	printf("*******This is a Number Guess Game!*****\n");
+	
+	while(Password != 1234)
+	{
+		if( i >= 3)
+		return;
+		i++;
+		puts("Please input a Password:");
+		scanf("%d",&Password);
+	}
+	while(Number != price)
+	{
+		do{
+			puts("Please input a number between 1 and 100: \n");
+			scanf("%d",&Number);
+			//printf("Your input Number is %d\n",Number);
+		} while(Number < 1 || Number > 100);
 
-    printf("\n====This is a Number Guess Game!====\n");
-
-    while (Password != 1234) {
-        if (i >= 3)
-            return 0;
-        i++;
-        printf("Please input Password: ");
-        scanf("%d", &Password);
-    }
-
-    i = 0;
-
-    while (Number != price) {
-        do {
-            printf("Please input a number between 1 and 100: ");
-            scanf("%d", &Number);
-            printf("Your input number is %d\n", Number);
-        } while (!(Number >= 1 && Number <= 100));
-
-        if (Number >= 90) {
-            printf("Too Bigger! Press any key to try again!\n");
-        } else if (Number >= 70 && Number < 90) {
-            printf("Bigger!\n");
-        } else if (Number >= 1 && Number <= 30) {
-            printf("Too Small! Press any key to try again!\n");
-        } else if (Number > 30 && Number <= 50) {
-            printf("Small! Press any key to try again!\n");
-        } else {
-            if (Number == price) {
-                printf("Cool, You are right!\n");
-            } else if (Number < price) {
-                printf("Sorry, Only a little smaller! Press any key to try again!\n");
-            } else if (Number > price) {
-                printf("Sorry, Only a little bigger! Press any key to try again!\n");
-            }
-        }
-        getchar();
-    }
-
-    return 0;
+		if (Number >= 90)
+		{
+			printf("Too Bigger! Press any key to try again!\n");
+		}else if (Number >= 70)
+		{
+			printf("Bigger!\n");
+		}else if (Number <= 30)
+		{
+			printf("Too Small! Press any key to try again!\n");
+		}else if (Number <= 50)
+		{
+			printf("Small! Press any key to try again!\n");
+		}else
+		{
+			if (Number == price)
+			{
+				printf("OK! You are right! Good Job!\n");
+			}
+			else if (Number < price)
+			{
+				printf("Sorry, Only a little smaller! Press any key to try again! \n");
+			}
+			else
+			{
+				printf("Sorry, Only a little bigger! Press any key to try again!\n");
+			}
+		}
+	}
+	return 0;
 }
